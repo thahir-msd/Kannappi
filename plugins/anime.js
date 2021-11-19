34c462e7bb05bb22b621b07efcd4522697896052
msd <92383459+thahir-msd@users.noreply.github.com>
2021-11-20 02:32:49 +0530

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -96,7 +96,7 @@ if (Config.WORKTYPE == 'private') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by PLK'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by THAHIR'})
 
     }));
 }
@@ -190,7 +190,7 @@ else if (Config.WORKTYPE == 'public') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , mimetype: Mimetype.jpg, caption: Config.AFN})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {quoted: message.data , mimetype: Mimetype.jpg, caption: 'Made by THAHIR'})
 
     }));
 }
