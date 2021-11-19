8e3bbfedc3d7867e702d63e064828da8989a0cbf
msd <92383459+thahir-msd@users.noreply.github.com>
2021-11-20 02:30:41 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -14,16 +14,16 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.GL}, (async (message
     await message.sendMessage(Buffer(rashi.data), MessageType.image, {quoted: message.data , thumbnail: fs.readFileSync('./image/r.jpg'), mimetype: Mimetype.png, caption: `*≈≈≈≈≈≈≈Links ☟︎︎︎≈≈≈≈≈≈≈≈*
  
 
-        *Github link        _https://tinyurl.com/yggyjfgf_*
+        *Github link        _https://github.com/thahir-msd_*
  
 
-       *Audio commads-1     _https://tinyurl.com/yfdkmuhd_*
+       *Audio commads-1     _https://github.com/thahir-msd/Kannappi/tree/master/upload_*
  
  
-       *Audio commads-2     _https://tinyurl.com/yejp9qkh_*
+       *Audio commads-2     _https://github.com/thahir-msd/Kannappi/tree/master/upload_*
   
   
-      *Sticker commads      _https://tinyurl.com/yejlp33d_*
+      *Sticker commads      _https://github.com/thahir-msd/Kannappi/tree/master/stickers_*
 `}) 
 
 })); 
